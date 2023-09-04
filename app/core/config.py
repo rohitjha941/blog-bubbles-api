b6b1210d11a4217cb924d77a5fac36741c08430f
Rohit Jha <me@rjha.dev>
2023-09-04 15:48:40 +0530

    allow crs origins to be string

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    BACKEND_CORS_ORIGINS: List[str] = []
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
